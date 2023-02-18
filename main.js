8e680550f0d71d8a1f8aacdfc6dde0e9108ce150
rajesh662 <rajkuttyrajesh56@gmail.com>
2023-02-18 18:51:19 +0530

    upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// let add = document.querySelector(".rk");
+// let add1=[add];
+// console.log(add1);
+
+// let add = document.createElement("p");
+// add.innerHTML="rajesh1";
+
+// let add1=[add];
+// console.log(add1);
+
+let n  = 123;
+while(n){
+    document.write(n);
+    --n;
+}
